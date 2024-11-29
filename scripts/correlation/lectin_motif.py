ab64dec9e518e7a2c323c7631d5b6adc96615cb1
zeyak <zeynep.hitay.akdeniz@gmail.com>
2024-11-29 14:28:43 +0100

    dev

--- scripts/correlation/lectin_motif.py
+++ scripts/correlation/lectin_motif.py
@@ -14,3 +14,5 @@ lectin_binding_motif = {
 for lectin, binding_motif in lectin_binding_motif.items():
     metric_df_instance = metric_df(lectin, binding_motif)
     plot_combined(metric_df_instance, lectin, binding_motif)
+
+##git
