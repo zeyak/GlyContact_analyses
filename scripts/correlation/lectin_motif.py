cf435f9ef5f5c1fd12e99d8b22d6f01b0d980531
zeyak <zeynep.hitay.akdeniz@gmail.com>
2024-11-29 13:57:47 +0100

    dev

--- scripts/correlation/lectin_motif.py
+++ scripts/correlation/lectin_motif.py
@@ -5,8 +5,9 @@ from scripts.correlation.func.plot_corr_regg import plot_combined
 lectin_binding_motif = {
     "AAL": ["Fuc"],
      "SNA": ["Neu5Ac(a2-6)", "Neu5Gc(a2-6)"],
+    "ConA": ["Man(a1-2)"],
      "MAL-II": ["Neu5Ac"],
-     "ConA": ["Man(a1-2)"],
+
      "PNA": ["Gal(b1-3)", "GalNAc"]
 }
 
