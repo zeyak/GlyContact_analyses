e4acb8fe49a2f4a016b6bb4ce2d30dc41581aadd
zeyak <zeynep.hitay.akdeniz@gmail.com>
2024-12-11 16:55:01 +0100

    metric_df_4, regg plots, properties, termini list

--- scripts/correlation/lectin_motif.py
+++ scripts/correlation/lectin_motif.py
@@ -1,25 +1,48 @@
-from scripts.correlation.func.metric_df4 import metric_df
+from scripts.correlation.func.metric_df import metric_df
 from scripts.correlation.func.plot_corr_regg import plot_combined_colors
 
 
+# Example usage
+
 lectin_binding_motif = {
-    "AOL": ["Fuc"],
-    "AAL": ["Fuc"],
-    "SNA": ["Sia(a2-6)"],
-    "ConA": ["Man"],
-    "MAL-II": ["Sia(a2-3)"],
-    "PNA": ["Gal(b1-3)GalNAc"],
-    "CMA": ["Fuc(a1-2)Gal", "GalNAc"],
-    "HPA": ["GalNAc(a1-?)", "GlcNAc(b1-?)"]
+    "AAL": {
+        "motif": ["Fuc"],
+        "termini_list": [["t"]]
+    },
+    "SNA": {
+        "motif": ["Sia(a2-6)"],
+        "termini_list": [["t"]]
+    },
+    "ConA": {
+        "motif": ["Man"],
+        "termini_list": [["t"]]
+    },
+    "MAL-II": {
+        "motif": ["Sia(a2-3)"],
+        "termini_list": [["t"]]
+    },
+    "PNA": {
+        "motif": ["Gal(b1-3)GalNAc"],
+        "termini_list": [["t", "f"]]
+    },
+    "CMA": {
+        "motif": ["Fuc(a1-2)Gal", "GalNAc"],
+        "termini_list": [["t", "f"], ["t"]]
+    },
+    "HPA": {
+        "motif": ["GalNAc(a1-?)", "GlcNAc(b1-?)"],
+        "termini_list": [["t"], ["t"]]
+    }
 }
 
 
 metric_df_ = {}
-for lectin, binding_motif in lectin_binding_motif.items():
+for lectin, properties in lectin_binding_motif.items():
     print("")
     print(f"Processing lectin: {lectin}")
+    print(f"Motif: {properties['motif']}")
+    print(f"Termini: {properties['termini_list']}")
 
-    metric_df_[lectin] = metric_df(lectin,binding_motif)
-    plot_combined_colors(metric_df_[lectin], lectin, binding_motif)
+    metric_df_[lectin] = metric_df(lectin,properties)
+    plot_combined_colors(metric_df_[lectin], lectin, properties["motif"])
 
-#test
